ecba10093bcd27f2c0a9d7985b9d3b1c3bd93684
janezhou309645512 <39550832+janezhou309645512@users.noreply.github.com>
2018-06-19 15:56:46 +0800

    2018-06-19 3

---
+++ src/main/java/com/ls/sbmdemo/util/DateTimeUtil.java
@@ -0,0 +1,29 @@
+package com.ls.sbmdemo.util;
+
+
+import java.util.Date;
+
+public class DateTimeUtil {
+    public static final String DATE_FORMAT="yyyy-MM-dd HH:mm:ss";
+    //将时间转换为字符串
+
+
+
+    //将字符串转化为时间
+
+    public static Date strToDate(String dataTimeStr){
+
+        return new Date();
+
+
+
+    }
+    public static String  dateToStr(Date date){
+        return "";
+    }
+
+
+
+
+
+}
